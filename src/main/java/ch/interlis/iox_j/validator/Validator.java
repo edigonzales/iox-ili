ce74ef04424d22e604890391a37779e81ca97ed8
edigonzales <edi.gonzales@gmail.com>
2026-01-10 16:33:36 +0100

    uniqueness-fix

--- src/main/java/ch/interlis/iox_j/validator/Validator.java
+++ src/main/java/ch/interlis/iox_j/validator/Validator.java
@@ -2261,12 +2261,16 @@ public class Validator implements ch.interlis.iox.IoxValidator {
                             }
 
                             IomObject targetObj = getReferencedObject(role, targetOid);
-                            if (targetObj != null) {
-                                nextCurrentObjects.add(targetObj);
-                            } else if (role.isExternal() && allObjectsAccessible && roleDefValue != null && k == lastPathIndex) {
+                            if (role.isExternal() && allObjectsAccessible && roleDefValue != null && k == lastPathIndex) {
                                 // if the target object is external and not available in the pool, keep the reference so
                                 // uniqueness checks and similar evaluations can still work with the OID.
-                                nextCurrentObjects.add(roleDefValue);
+								if (role instanceof RoleDef) {
+									nextCurrentObjects.add(roleDefValue);
+								} else {
+									nextCurrentObjects.add(targetObj);
+								}
+                            } else if (targetObj != null) {
+                                nextCurrentObjects.add(targetObj);
                             }
                         }
                     }
